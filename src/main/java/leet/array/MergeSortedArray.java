b008fa28f262bf0ebee537eca8c747b5c0c7a749
SeanLiew <471293295@qq.com>
2020-06-01 16:15:51 +0800

    二叉树

--- src/main/java/leet/array/MergeSortedArray.java
+++ src/main/java/leet/array/MergeSortedArray.java
@@ -1,5 +1,6 @@
 package leet.array;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
 /**
@@ -11,24 +12,49 @@ public class MergeSortedArray {
     @Test
     public void test(){
         int[] arr1 = {1, 2, 3, 0, 0, 0};
-        int[] arr2 = {2 ,4, 6};
+        int[] arr2 = {1 ,4, 6};
+//        int[] arr1 = {0,0,0};
+//        int[] arr2 = {1,2,3};
+//        int[] arr1 = {2,0};
+//        int[] arr2 = {1};
         merge(arr1, 3, arr2, 3);
 
-        System.out.println(arr1);
+        System.out.println(JSON.toJSONString(arr1));
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int pointer = 0;
-
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++){
             int insert = nums2[i];
-            if (insert >= nums1[pointer]){
-                continue;
+            for(int j = m; j >= 0; j--){
+                if (j == 0){
+                    nums1[j] = insert;
+                    break;
+                }else if (insert >= nums1[j-1]) {
+                    nums1[j] = insert;
+                    break;
+                } else {
+                    nums1[j] = nums1[j-1];
+                    nums1[j-1] = 0;
+                }
             }
-            while(true){
-
-
-            }
+            m++;
         }
 
+    }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
     }
 }
---
+++ src/main/java/leet/tree/TwoTree.java
@@ -0,0 +1,50 @@
+package leet.tree;
+
+
+import org.junit.Test;
+
+/**
+ * desc:
+ * author: liuxiaozheng
+ * time: 2020/6/1  15:41
+ **/
+public class TwoTree {
+
+    @Test
+    public void test(){
+        TreeNode treeNode1 = new TreeNode(3);
+        TreeNode leftNode1 = new TreeNode(1);
+        TreeNode rightNode1 = new TreeNode(2);
+        treeNode1.left = leftNode1;
+        treeNode1.right = rightNode1;
+
+        TreeNode treeNode2 = new TreeNode(3);
+        TreeNode leftNode2 = new TreeNode(1);
+        TreeNode rightNode2 = new TreeNode(2);
+        treeNode2.left = leftNode2;
+        treeNode2.right = rightNode2;
+
+
+        System.out.println(isSameTree(treeNode1,treeNode2 ));
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null){
+            return false;
+        }
+        if (p.val != q.val){
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+   }
+}
