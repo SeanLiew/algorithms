cb4bd668bb0464c157d0c422d8bc9f527eae1238
SeanLiew <471293295@qq.com>
2020-08-11 17:48:02 +0800

    数组

--- src/main/java/leet/nums/Rotate.java
+++ src/main/java/leet/nums/Rotate.java
@@ -18,13 +18,20 @@ public class Rotate {
     }
 
     public void rotate(int[] nums, int k) {
-        for (int i = 0; i < nums.length; i++){
-            int dest = i + k;
-            if (dest >= nums.length){
-                dest -= nums.length;
-            }
-            int temp = nums[dest];
-            nums[dest] = nums[i];
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
