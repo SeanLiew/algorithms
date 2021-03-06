80b2c7e2f6e349420d5e58597e34cdbd9b44af72
liuxiaozheng3 <liuxiaozheng3@jd.com>
2021-03-06 19:57:19 +0800

    test

--- src/main/java/leet/strs/SpaceChange.java
+++ src/main/java/leet/strs/SpaceChange.java
@@ -6,4 +6,15 @@ package leet.strs;
  * @Created: 2021-03-06-19:38
  */
 public class SpaceChange {
+    @Test
+    public void test(){
+        String strt = "";
+        String result = replaceSpace(strs);
+        System.out.println(result);
+
+    }
+
+    public String replaceSpace(String s) {
+
+    }
 }
