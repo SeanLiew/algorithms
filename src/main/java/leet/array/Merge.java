3f55452bcd897a6c63b0e3fa267c3b93a3509cbb
liuxiaozheng02 <liuxiaozheng02@meituan.com>
2024-04-11 22:55:41 +0800

    sort

--- src/main/java/leet/array/Merge.java
+++ src/main/java/leet/array/Merge.java
@@ -23,12 +23,67 @@ public class Merge {
 
 
     @Test
-    public void test2 (){
+    public void test1 (){
         int[] nums1 = {4,5,6,0,0,0};
         int m = 3;
         int[] nums2 = {1,2,3};
         int n = 3;
         merge(nums1, m ,nums2, n);
         System.out.println(JSON.toJSON(nums1));
+    }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length - 1;
+        int a = m - 1;
+        int b = n - 1;
+        while (a >= 0 || b >= 0) {
+            if (a >=0 && (b < 0 || nums1[a] > nums2[b])) {
+                nums1[index--] = nums1[a];
+                a--;
+            }
+            if (b >=0 && (a < 0 || nums1[a] <= nums2[b])) {
+                nums1[index--] = nums2[b];
+                b--;
+            }
+        }
+    }
+
+
+    @Test
+    public void test2 (){
+        int[] nums1 = {4,5,7,0,0,0};
+        int m = 3;
+        int[] nums2 = {1,2,6};
+        int n = 3;
+        merge2(nums1, m ,nums2, n);
+        System.out.println(JSON.toJSON(nums1));
+    }
+
+    public int[] merge3(int[] nums1, int[] nums2) {
+
+        int [] result = new int[nums1.length + nums2.length];
+
+        int a = 0;
+        int b = 0;
+        while(a < nums1.length || b < nums2.length) {
+            if (a < nums1.length && (b == nums2.length || nums1[a] < nums2[b])) {
+                result[a+b] = nums1[a];
+                a++;
+            }
+            if (b < nums2.length && (a == nums1.length || nums1[a] >= nums2[b])) {
+                result[a+b] = nums2[b];
+                b++;
+            }
+        }
+
+        return result;
+    }
+
+
+    @Test
+    public void test3 (){
+        int[] nums1 = {3,4,5,8};
+        int[] nums2 = {1,2,6,7};
+        System.out.println(JSON.toJSON(merge3(nums1,nums2)));
     }
 }
