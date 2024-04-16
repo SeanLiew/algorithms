2a4411292567599feb9345f4dbc20478c5f5fcb5
liuxiaozheng02 <liuxiaozheng02@meituan.com>
2024-04-16 21:21:28 +0800

    t

---
+++ src/main/java/leet/bits/ReverseBits.java
@@ -0,0 +1,22 @@
+package leet.bits;
+
+import org.junit.Test;
+
+/**
+ * @description:
+ * @author: liuxiaozheng
+ * @date 2021-11-12 3:17 下午
+ */
+public class ReverseBits {
+    @Test
+    public void test(){
+        int result = reverseBits(3);
+
+    }
+
+    public int reverseBits(int n) {
+
+        return 0;
+    }
+
+}
