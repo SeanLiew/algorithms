e3fe67ccb318b7ded490e3925fd1da2769ce8ad0
liuxiaozheng <liuxiaozheng02@meituan.com>
2024-04-24 20:06:18 +0800

    min

--- src/main/java/leet/nums/MinSubArrayLen.java
+++ src/main/java/leet/nums/MinSubArrayLen.java
@@ -1,5 +1,7 @@
 package leet.nums;
 
+import org.junit.Test;
+
 /**
  * @description:
  * @author: liuxiaozheng
@@ -7,7 +9,45 @@ package leet.nums;
  */
 public class MinSubArrayLen {
     public int minSubArrayLen(int target, int[] nums) {
+        if (nums.length == 1) {
+            return nums[0] == target ? 1 : 0;
+        }
 
-        return 0;
+        int left = 0;
+        int right = 0;
+        int minStep = 0;
+        while (left < nums.length) {
+            int sum = 0;
+            for (int i = left; i <= right; i++) {
+                sum += nums[i];
+            }
+            if (sum >= target) {
+                if (minStep == 0) {
+                    minStep = right - left + 1;;
+                } else{
+                    minStep = Math.min(minStep, right - left + 1);
+                }
+            }
+
+            if (sum >= target) {
+                left++;
+            }
+            if (sum < target) {
+                if (right < nums.length - 1) {
+                    right++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return minStep;
+    }
+
+    @Test
+    public void test1(){
+        System.out.println(minSubArrayLen(11, new int[] {1,2,3,4,5}));
+        System.out.println(minSubArrayLen(11, new int[] {1,1,1}));
+        System.out.println(minSubArrayLen(7, new int[] {2,3,1,2,4,3}));
+        System.out.println(minSubArrayLen(4, new int[] {1,4,4}));
     }
 }
