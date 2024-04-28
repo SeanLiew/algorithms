b96e14fe2126ae535074bfec1103e9c5311e2a39
liuxiaozheng02 <liuxiaozheng02@meituan.com>
2024-04-28 09:26:26 +0800

    digits

---
+++ src/main/java/leet/nums/PlusOne.java
@@ -0,0 +1,16 @@
+package leet.nums;
+
+import org.junit.Test;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+        return digits;
+    }
+
+    @Test
+    public void test(){
+        int[] nums = {4,5,6,3,2,1};
+        System.out.println(plusOne(nums));
+    }
+}
