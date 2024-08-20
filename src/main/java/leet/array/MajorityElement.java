ba10e2d03174e18863c45e9668cc3e1f3731b8d3
liuxiaozheng <liuxiaozheng02@meituan.com>
2024-08-20 18:40:28 +0800

    majority

--- src/main/java/leet/array/MajorityElement.java
+++ src/main/java/leet/array/MajorityElement.java
@@ -13,21 +13,21 @@ import java.util.Map;
 public class MajorityElement {
     @Test
     public void test(){
-        int[] arr = {4,2,4,4,4,5,5};
-        int i = majorityElement(arr);
-
-        System.out.println(i);
+        System.out.println(majorityElement(new int[] {4,2,2,4,2}));
+        System.out.println(majorityElement(new int[] {4,2,4,4,4,5,5}));
     }
 
     public int majorityElement(int[] nums) {
         int curNum = nums[0];
         int vote = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (vote == 0){
+        for (int i = 0; i < nums.length; i++) {
+            if (vote == 0) {
                 curNum = nums[i];
             }
-            vote += curNum == nums[i] ? 1 : -1;
+            vote += (curNum == nums[i]) ? 1 : -1;
         }
         return curNum;
     }
+
+
 }
