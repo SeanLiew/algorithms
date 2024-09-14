50df7cf1db6803e35ab92308f7718c90ba3ffaaf
liuxiaozheng02 <liuxiaozheng02@meituan.com>
2024-09-14 18:57:56 +0800

    t

---
+++ src/main/java/leet/dp/Tribonacci.java
@@ -0,0 +1,29 @@
+package leet.dp;
+
+import org.junit.Test;
+
+public class Tribonacci {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for ( int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+    @Test
+    public void test(){
+        System.out.println(tribonacci(4));
+    }
+}
