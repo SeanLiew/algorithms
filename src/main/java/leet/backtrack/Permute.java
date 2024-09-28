920a7e26a42c531fefd3ce3d1c6fec8adfc9a7e0
liuxiaozheng02 <liuxiaozheng02@meituan.com>
2024-09-28 21:14:21 +0800

    t

--- src/main/java/leet/backtrack/Permute.java
+++ src/main/java/leet/backtrack/Permute.java
@@ -4,12 +4,35 @@ import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Permute {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permute3(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result,  nums, 0, nums.length - 1, new ArrayList<>());
+        return result;
+    }
+    private void backtrack(List<List<Integer>> result, int[] nums, int start, int end, List<Integer> temp) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+        } else {
+            for (int i = start; i <= end; i++) {
+                int tempIndex = i;
+                if (i >= nums.length) {
+                    tempIndex = tempIndex % nums.length;
+                }
+                temp.add(nums[tempIndex]);
+                backtrack(result,  nums, i + 1, i + nums.length - temp.size(), temp);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+
+    public List<List<Integer>> permute2(int[] nums) {
 
         List<List<Integer>> result = new ArrayList<>();
         backtrack(result,  nums, new HashSet<>(), new ArrayList<>());
@@ -29,6 +52,34 @@ public class Permute {
                 temp.remove(temp.size() - 1);
                 set.remove(num);
             }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
         }
     }
 
